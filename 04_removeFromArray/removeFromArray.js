fb034ed318b224d8d1ea243cd2879880be1ced4d
Aaron Chandler <41349277+AaronChandler976@users.noreply.github.com>
2025-01-09 23:26:44 -0800

    Complete removeFromArray function Added function to find and remove multiple array elements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numsToRemove) {
+    let finalArr = arr;
+    for (const num of numsToRemove) {
+        let idx = finalArr.indexOf(num)
+        while (idx > -1) {
+            finalArr.splice(idx, 1);
+            idx = finalArr.indexOf(num);
+        }
+    }
+    return finalArr;
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
