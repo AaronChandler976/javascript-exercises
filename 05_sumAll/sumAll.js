dabb49940ed44de434f15028ca0050b7b5117569
Aaron Chandler <41349277+AaronChandler976@users.noreply.github.com>
2025-01-09 23:42:07 -0800

    Complete sumAll function Completed sumAll function, passes ERROR for non, negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    for (const arg of arguments) {
+        if (typeof arg !== 'number' || !Number.isInteger(arg) || arg < 0) return 'ERROR';
+    }
 
+    let finalSum = 0;
+    let small = num1 < num2 ? num1 : num2;
+    let large = num1 > num2 ? num1 : num2;
+    while (small <= large) {
+        finalSum += small;
+        small++;
+    }
+    return finalSum;
 };
+
+sumAll(123, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
