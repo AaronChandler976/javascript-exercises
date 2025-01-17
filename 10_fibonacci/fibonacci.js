9f4bb7e25788eb519cf782c1961d99248dca3183
Aaron Chandler <41349277+AaronChandler976@users.noreply.github.com>
2025-01-16 19:51:17 -0800

    Complete exercise 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) return "OOPS";
+    let fibArr = [0, 1, 1];
+    for (let i = 2; i < n; i++) {
+        fibArr.push(fibArr[i] + fibArr[i-1]);
+    }
+    return fibArr[n];
 };
 
 // Do not edit below this line
