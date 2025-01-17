a53847aa5a4de271cec7b1e741d843a8db1bd5eb
Aaron Chandler <41349277+AaronChandler976@users.noreply.github.com>
2025-01-16 20:03:08 -0800

    Complete exercise 12 find oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((acc, p) => getAge(acc) > getAge(p) ? acc : p);
+};
 
+function getAge(person) {
+    return (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
 };
 
 // Do not edit below this line
