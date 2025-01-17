0036b61d094d2baf53568cc4d53d6ecfaf7b4388
Aaron Chandler <41349277+AaronChandler976@users.noreply.github.com>
2025-01-16 19:30:14 -0800

    Complete exercises 08, 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const regex = new RegExp(/[a-zA-z0-9]/);
+    const characters = word.toLowerCase().split("").filter(c => regex.test(c));
+    console.log(characters);
+    let lo = 0;
+    let hi = characters.length - 1;
+    while (lo < hi) {
+        if (characters[lo] !== characters[hi]) return false;
+        lo++; hi--;
+    }
+    return true;
 };
 
 // Do not edit below this line
